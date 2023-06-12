b2ec8235a9e28bd0e764505f8d0fcbdfaebd1d6c
Thiago Weslley <thiagoweslley@yahoo.com>
2023-06-12 11:23:49 -0300

    feat: Adiciona validação para geração do número

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,16 +5,20 @@ const generateNumber = () => {
     const max = parseInt(document.querySelector('#last-number').value);
     const drawnNumber = document.querySelector('#drawn-number');
 
-    const range = max - min + 1;
-    const randomNumber = Math.floor(Math.random() * range) + min;
+    if (min < max) {
+        const range = max - min + 1;
+        const randomNumber = Math.floor(Math.random() * range) + min;
 
-    drawnNumber.innerHTML = randomNumber;
+        drawnNumber.innerHTML = randomNumber;
 
-    drawnNumber.classList.add('animate');
+        drawnNumber.classList.add('animate');
 
-    setTimeout(() => {
-        drawnNumber.classList.remove('animate');
-    }, 640);
+        setTimeout(() => {
+            drawnNumber.classList.remove('animate');
+        }, 640);
+    } else {
+        alert('O segundo número deve ser maior que o primeiro número');
+    }
 }
 
 raffleButton.addEventListener('click', generateNumber);
